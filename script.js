481ee84df4519ecae638db2f65694aa185270636
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-30 23:10:21 +0530

    doc added

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+//DOM references for updating content during runtime
 const waterAnimation = document.getElementById("water-status-animation");
 const waterGlasses = document.querySelectorAll(".water-glass");
 const waterStatusText = document.querySelector(".water-status-text");
 const success = document.querySelector(".success");
 let intervalReference;
 
+//Variable declaration for volume calculations
 let totalToDrink = 2 * 250 * 4;
 let totalMls = 0;
 let percentagePrevious = 0;
 let percentageCurrent = 0;
 
+//For each water glass, When user clicks on it, Scale them out and add to main volume drank
 waterGlasses.forEach((glass) => {
     glass.addEventListener("click", () => {
         totalMls += 250;
@@ -25,6 +28,10 @@ waterGlasses.forEach((glass) => {
     });
 });
 
+/**
+ * @author Jeeva Kalaiselvam
+ * @param {Number} number - This defines the percentage to be updated in main volume consumption data
+ */
 function updatePercentage(number) {
     if (number + 1 >= 99) {
         waterStatusText.innerHTML = `100 %`;
